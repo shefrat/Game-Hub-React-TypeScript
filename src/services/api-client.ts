92609511a4d79ee617cc617650b546efcff0cdc7
Hichem Razgallah <hichemraz@gmail.com>
2023-12-05 16:35:52 +0800

    Add comment about the key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: '821d26c34bb44ca6b03964d2eb515fe1'
+        key: '821d26c34bb44ca6b03964d2eb515fe1' // It doesn't work, get you key from https://rawg.io/
     }
 });
