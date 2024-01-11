6697290f0240fda9d1bb33734a718f72a39ed8b8
Hichem Razgallah <hichemraz@gmail.com>
2024-01-11 16:11:05 +0800

    Setup Routing

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,42 +1,28 @@
-import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
-import GameGrid from "./components/GameGrid";
-import GameHeading from "./components/GameHeading";
-import GenreList from "./components/GenreList";
-import NavBar from "./components/NavBar";
-import PlatformSelector from "./components/PlatformSelector";
-import SortSelector from "./components/SortSelector";
+import { Grid, Show, GridItem, Flex } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import GameGrid from "../components/GameGrid";
+import GameHeading from "../components/GameHeading";
+import GenreList from "../components/GenreList";
+import PlatformSelector from "../components/PlatformSelector";
+import SortSelector from "../components/SortSelector";
 
-export interface GameQuery {
-  genreId?: number;
-  platformId?: number;
-  sortOrder: string;
-  searchText: string;
-}
-
-function App() {
-  // const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
-
-  // const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
-  // const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
-  //   null
-  // );
-
+const HomePage = () => {
   return (
     <Grid
       templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
+        base: `"main"`,
+        lg: `"aside main"`,
       }}
       templateColumns={{
         base: "1fr",
         lg: "200px 1fr",
       }}
     >
-      <GridItem area="nav">
+      {/* <GridItem area="nav">
         <NavBar
         // onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
         />
-      </GridItem>
+      </GridItem> */}
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
           <GenreList
@@ -75,6 +61,6 @@ function App() {
       </GridItem>
     </Grid>
   );
-}
+};
 
-export default App;
+export default HomePage;
