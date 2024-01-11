f70b903c05463805fb1215ea632c275665e3bf7f
Hichem Razgallah <hichemraz@gmail.com>
2024-01-11 13:03:05 +0800

    Refactor - manage state with zustand

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -3,14 +3,17 @@ import { BsChevronDown } from "react-icons/bs";
 import usePlatforms from "../hooks/usePlatforms";
 import { Platform } from "../hooks/usePlatforms";
 import usePlatform from "../hooks/usePlatform";
+import useGameQueryStore from "../store";
 
-interface Props {
-  onSelectPlatform: (platform: Platform) => void;
-  selectedPlatformId?: number;
-}
+// interface Props {
+//   onSelectPlatform: (platform: Platform) => void;
+//   selectedPlatformId?: number;
+// }
 
-const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
+const PlatformSelector = () => {
   const { data, error } = usePlatforms();
+  const setSelectedPlatformId = useGameQueryStore((s) => s.setPlatformId);
+  const selectedPlatformId = useGameQueryStore((s) => s.gameQuery.platformId);
 
   const selectedPlatform = usePlatform(selectedPlatformId);
 
@@ -23,7 +26,7 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
       <MenuList>
         {data?.results.map((platform) => (
           <MenuItem
-            onClick={() => onSelectPlatform(platform)}
+            onClick={() => setSelectedPlatformId(platform.id)}
             key={platform.id}
           >
             {platform.name}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,8 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
-import { GameQuery } from "../App";
-import APIClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client";
-import { Platform } from "./usePlatforms";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import ms from "ms";
+import APIClient, { FetchResponse } from "../services/api-client";
+import useGameQueryStore from "../store";
+import { Platform } from "./usePlatforms";
 
 export interface Game {
     id: number;
@@ -16,23 +15,26 @@ export interface Game {
 
 const apiClient = new APIClient<Game>('/games');
 
-const useGames = (gameQuery: GameQuery) => useInfiniteQuery<FetchResponse<Game>, Error>({
-    queryKey: ["games", gameQuery],
-    queryFn: ({ pageParam = 1 }) =>
-        apiClient
-            .getAll({
-                params: {
-                    genres: gameQuery.genreId,
-                    parent_platforms: gameQuery.platformId,
-                    ordering: gameQuery.sortOrder,
-                    search: gameQuery.searchText,
-                    page: pageParam
-                },
-            }),
-    getNextPageParam: (lastPage, allPages) => {
-        return lastPage.next ? allPages.length + 1 : undefined;
-    },
-    staleTime: ms('24h')
-})
+const useGames = () => {
+    const gameQuery = useGameQueryStore(s => s.gameQuery);
+    return useInfiniteQuery<FetchResponse<Game>, Error>({
+        queryKey: ["games", gameQuery],
+        queryFn: ({ pageParam = 1 }) =>
+            apiClient
+                .getAll({
+                    params: {
+                        genres: gameQuery.genreId,
+                        parent_platforms: gameQuery.platformId,
+                        ordering: gameQuery.sortOrder,
+                        search: gameQuery.searchText,
+                        page: pageParam
+                    },
+                }),
+        getNextPageParam: (lastPage, allPages) => {
+            return lastPage.next ? allPages.length + 1 : undefined;
+        },
+        staleTime: ms('24h')
+    })
+}
 
 export default useGames;
